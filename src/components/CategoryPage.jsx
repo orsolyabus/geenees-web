082ed6c0e40afe640bb6e5c524a505bb3c8c59d5
Orsolya Torokne Bus <orsolyabus@gmail.com>
2019-12-11 10:45:37 -0800

    get products from endpoint

--- src/components/CategoryPage.jsx
+++ src/components/CategoryPage.jsx
@@ -1,17 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Product from './Product';
 
-function CategoryPage() {
-  const products = [1,2,3]
-  
-  return (
-    <div>
-      <h1>Baby products</h1>
-      {products.map(product => (
-        <Product key={product} />
-      ))}
-    </div>
-  )
+class CategoryPage extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      products: []
+    };
+  }
+
+  componentDidMount() {
+    const URL = "https://stgapi.geenees.co/1.0/store/ae29bef1-9104-4340-b80d-fffdc663c9a7/categories/3561347011"
+
+    fetch(URL, {
+      method: "GET",
+      headers: {
+        "geenee-session": localStorage.getItem("geenee-session")
+      }
+    })
+      .then(res => res.json())
+      // TODO: handle errors
+      .then(res => {
+        const products = res.data.search_results.items
+        this.setState({
+          products: products,
+          loading: false
+        });
+      })
+  }
+
+  render() {
+    const { loading, products } = this.state;
+    if (loading) {
+      return (
+        <main className="QuestionIndexPage">
+          <h1>Baby Products</h1>
+          <h2>Loading...</h2>
+        </main>
+      )
+    }
+    return (
+      <div>
+        <h1>Baby products</h1>
+        {products.map(product => (
+          <Product key={product.asin} product={product} />
+        ))}
+      </div>
+    )
+  }
 }
 
 export default CategoryPage;
